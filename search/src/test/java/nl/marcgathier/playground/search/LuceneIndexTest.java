f8aa74ee91f73b868fdce503369c19c01442b22e
Marc <marcgathier@gmail.com>
2013-04-11 10:45:42 +0200

    Initial version

---
+++ search/src/test/java/nl/marcgathier/playground/search/LuceneIndexTest.java
@@ -0,0 +1,72 @@
+package nl.marcgathier.playground.search;
+
+import java.io.IOException;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopScoreDocCollector;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.apache.lucene.util.Version;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LuceneIndexTest {
+	static Directory index = new RAMDirectory();
+	static StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_42);
+
+	@BeforeClass
+	public static void setupIndex() throws Exception {
+
+		IndexWriterConfig config = new IndexWriterConfig(Version.LUCENE_42,
+				analyzer);
+		IndexWriter w = new IndexWriter(index, config);
+		addDoc(w, "Lucene in Action", "193398817");
+		addDoc(w, "Lucene for Dummies", "55320055Z");
+		addDoc(w, "Managing Gigabytes", "55063554A");
+		addDoc(w, "The Art of Computer Science", "9900333X");
+		w.close();
+	}
+
+	private static void addDoc(IndexWriter w, String title, String isbn)
+			throws IOException {
+		Document doc = new Document();
+		doc.add(new TextField("title", title, Field.Store.YES));
+		doc.add(new StringField("isbn", isbn, Field.Store.YES));
+		w.addDocument(doc);
+	}
+
+	@Test
+	public void search() throws IOException, ParseException {
+		 Query q = new QueryParser(Version.LUCENE_42, "title", analyzer).parse("dummy");
+
+		int hitsPerPage = 10;
+		IndexReader reader = DirectoryReader.open(index);
+		IndexSearcher searcher = new IndexSearcher(reader);
+		TopScoreDocCollector collector = TopScoreDocCollector.create(hitsPerPage, true);
+		searcher.search(q, collector);
+		ScoreDoc[] hits = collector.topDocs().scoreDocs;
+		assertEquals(2, hits.length);
+		
+		for(int i=0;i<hits.length;++i) {
+		      int docId = hits[i].doc;
+		      Document d = searcher.doc(docId);
+		      System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+		    }
+
+		reader.close();
+	}
+}
